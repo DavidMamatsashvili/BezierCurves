cb1f2a352d0d381a353578e793a014b7d87e9d07
DavidMamatsashvili <mamatsashvilidavid426@gmail.com>
2026-07-15 01:30:28 +0400

    added lerp and decasteljau

--- BezierCurves/Program.cs
+++ BezierCurves/Program.cs
@@ -5,16 +5,38 @@ namespace BezierCurves
 {
     public class Program
     {
+        public static Vector2 Lerp(Vector2 pointA, Vector2 pointB, float t)
+        {
+            return new Vector2(pointA.X+(pointB.X-pointA.X)*t, pointA.Y+(pointB.Y-pointA.Y)*t);
+        }
+
+        public static Vector2 DeCasteljau(List<Vector2> points, float t)
+        {
+            while (points.Count > 1)
+            {
+                List<Vector2> next = new List<Vector2>();
+                for(int i=0;i<points.Count-1; i++)
+                {
+                    next.Add(Lerp(points[i], points[i+1], t));
+                }
+
+                points = next;
+            }
+            return points.FirstOrDefault();
+        }
+
         static void Main(string[] args)
         {
             const int width = 800;
             const int height = 480;
-            const string text = "bezier curves";
+            const string text = "Bezier Curves";
             Raylib.InitWindow(width,height,text);
+            Raylib.SetTargetFPS(60);
+
             List<Vector2> rectangles = new List<Vector2>();
 
             int markedRectangle = -1;
-
+            
             while (!Raylib.WindowShouldClose())
             {
                 if (Raylib.IsMouseButtonPressed(MouseButton.Left))
@@ -47,10 +69,25 @@ namespace BezierCurves
 
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.White);
+
+                Vector2 previous = DeCasteljau(rectangles, 0.0f);
                 for (int i = 0; i < rectangles.Count; i++)
                 {
                     Raylib.DrawRectangle((int)rectangles[i].X, (int)rectangles[i].Y, 20, 20, Color.Red);
                 }
+
+                for(int i =rectangles.Count-1; i >=1; i--)
+                {
+                    Raylib.DrawLineDashed(new Vector2(rectangles[i].X+10, rectangles[i].Y+10), new Vector2(rectangles[i-1].X+10, rectangles[i-1].Y+10), 15, 15, Color.Red);
+                }
+
+                for (float t = 0.01f; t <= 1.0f; t += 0.001f)
+                {
+                    Vector2 current = DeCasteljau(rectangles, t);
+                    Raylib.DrawLine((int)previous.X + 10, (int)previous.Y + 10, (int)current.X + 10, (int)current.Y + 10, Color.Blue);
+                    previous = current;
+                }
+
                 Raylib.EndDrawing();
             }
 
