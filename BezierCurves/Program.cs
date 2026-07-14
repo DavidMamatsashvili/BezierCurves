29fba9b4a73975c43db5b79f0c92ddf7b083eeb4
DavidMamatsashvili <mamatsashvilidavid426@gmail.com>
2026-07-14 20:49:46 +0400

    cleaning

--- BezierCurves/Program.cs
+++ BezierCurves/Program.cs
@@ -5,13 +5,6 @@ namespace BezierCurves
 {
     public class Program
     {
-        //TODO
-        //fix two rectangle collision. if two rectangles overlap each other, they merge into one
-
-        //TODO
-        //add lerp function
-
-        //implement bezier curves
         static void Main(string[] args)
         {
             const int width = 800;
@@ -19,7 +12,7 @@ namespace BezierCurves
             const string text = "bezier curves";
             Raylib.InitWindow(width,height,text);
             List<Vector2> rectangles = new List<Vector2>();
-            Vector2 rightMousePos = new Vector2();
+
             int markedRectangle = -1;
 
             while (!Raylib.WindowShouldClose())
@@ -28,11 +21,6 @@ namespace BezierCurves
                 {
                     rectangles.Add(new Vector2(Raylib.GetMouseX(), Raylib.GetMouseY()));
                 }
-
-                //if (Raylib.IsMouseButtonPressed(MouseButton.Right))
-                //{
-                //    rightMousePos = new Vector2(Raylib.GetMouseX(), Raylib.GetMouseY());
-                //}
 
                 if (Raylib.IsMouseButtonPressed(MouseButton.Right))
                 {
@@ -61,42 +49,8 @@ namespace BezierCurves
                 Raylib.ClearBackground(Color.White);
                 for (int i = 0; i < rectangles.Count; i++)
                 {
-                    Raylib.DrawRectangle(
-                        (int)rectangles[i].X,
-                        (int)rectangles[i].Y,
-                        20,
-                        20,
-                        Color.Red);
+                    Raylib.DrawRectangle((int)rectangles[i].X, (int)rectangles[i].Y, 20, 20, Color.Red);
                 }
-
-                //for (int i = 0; i < rectangles.Count; i++)
-                //{
-                //    if (Raylib.IsMouseButtonDown(MouseButton.Right)
-                //        && rightMousePos.X >= rectangles[i].X && rightMousePos.X <= rectangles[i].X + 20
-                //        && rightMousePos.Y >= rectangles[i].Y && rightMousePos.Y <= rectangles[i].Y + 20
-                //        )
-                //    {
-                //        markedRectangle = i;
-                //        Raylib.DrawRectangle(Raylib.GetMouseX() - 10, Raylib.GetMouseY() - 10, 20, 20, Color.Red);
-                //        break;
-                //    }
-                //    else if (Raylib.IsMouseButtonReleased(MouseButton.Right)
-                //        //&& rightMousePos.X >= rectangles[i].X && rightMousePos.X <= rectangles[i].X + 20
-                //        //&& rightMousePos.Y >= rectangles[i].Y && rightMousePos.Y <= rectangles[i].Y + 20
-                //        && markedRectangle == i 
-                //        )
-                //    {
-                //        Raylib.DrawRectangle((int)rectangles[i].X - 10, (int)rectangles[i].Y - 10, 20, 20, Color.Red);
-                //        markedRectangle = -1;
-                //        rectangles[i] = new Vector2(Raylib.GetMouseX() - 10, Raylib.GetMouseY() - 10);
-                //    }
-                //    else
-                //    {
-                //        Raylib.DrawRectangle((int)rectangles[i].X, (int)rectangles[i].Y, 20, 20, Color.Red);
-                //    }
-                //}
-
-
                 Raylib.EndDrawing();
             }
 
